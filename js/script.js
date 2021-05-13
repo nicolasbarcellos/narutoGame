185aa7f4621bd502d908a983aaf5eff9c3556c41
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2021-05-13 13:30:07 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,35 +1,48 @@
 
 const x = window.matchMedia("(max-width: 1070px)");
+const mobile = window.matchMedia("(max-width: 750px)");
 
 const menuMobile = document.querySelector('.menuMobile');
 const menu = document.querySelector('.menu');
 const panel = document.querySelector('.panel');
+const logo = document.querySelector('.logo img');
+const social = document.querySelector('.social');
+const flags = document.querySelector('.langIcons');
 
 function test(x) {
   if (x.matches) {
     menuMobile.style.display = 'inline-block';
     menu.style.display = 'none';
 
-   menuMobile.addEventListener('click', () => {
-     if (panel.classList.contains('show')) {
-      panel.classList.remove('show')
-      menuMobile.classList.remove('round');
-     } 
-     else {
-      menuMobile.classList.add('round');
-      panel.classList.add('show');
-     } 
-   });
-
-
   } else {
     menuMobile.style.display = 'none';
     menu.style.display = 'flex';
   }
+
+  if (mobile.matches) {
+    social.style.display = 'none';
+    flags.style.display = 'none';
+    logo.src = 'assets/logo-mobile.png';
+  }  else {
+    logo.src = 'assets/logo.png';
+  }
 }
 
-// function showPanel {
-//   console.log('object');
-// }
 
-x.addListener(test)
+function aaa() {
+
+    if (panel.classList.contains('show')) {
+     panel.classList.remove('show')
+     menuMobile.classList.remove('round');
+    } 
+    else {
+     menuMobile.classList.add('round');
+     panel.classList.add('show');
+    } 
+}
+
+menuMobile.addEventListener('click', aaa);
+x.addEventListener('change', test);
+mobile.addEventListener('change', test);
+test(x);
+test(mobile);
