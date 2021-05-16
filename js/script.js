bc5784b83023e901a1368a0ec26c1fc7bf618e45
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2021-05-16 20:57:10 -0300

    slider-news

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 const x = window.matchMedia("(max-width: 1070px)");
 const mobile = window.matchMedia("(max-width: 750px)");
+const newsSwiper = window.matchMedia("(max-width: 1068px)");
 
 const menuMobile = document.querySelector('.menuMobile');
 const menu = document.querySelector('.menu');
@@ -8,6 +9,8 @@ const panel = document.querySelector('.panel');
 const logo = document.querySelector('.logo img');
 const social = document.querySelector('.social');
 const flags = document.querySelector('.langIcons');
+const news = document.querySelector('main');
+const mySwiper = document.querySelector('.mySwiper');
 
 function test(x) {
   if (x.matches) {
@@ -19,12 +22,20 @@ function test(x) {
     menu.style.display = 'flex';
   }
 
+  // if (newsSwiper.matches) {
+  //   news.style.display = 'none';
+  // } else {
+  //   mySwiper.style.display = 'block';
+  // }
+
   if (mobile.matches) {
     social.style.display = 'none';
     flags.style.display = 'none';
     logo.src = 'assets/logo-mobile.png';
   }  else {
     logo.src = 'assets/logo.png';
+    social.style.display = 'flex';
+    flags.style.display = 'flex';
   }
 }
 
@@ -41,8 +52,19 @@ function aaa() {
     } 
 }
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 menuMobile.addEventListener('click', aaa);
 x.addEventListener('change', test);
 mobile.addEventListener('change', test);
+// newsSwiper.addEventListener('change', test);
 test(x);
 test(mobile);
+test(newsSwiper);
